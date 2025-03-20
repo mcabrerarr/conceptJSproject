748b3fe436035a8bc48fc76a92358f8f62de0beb
Cabrera, Martin <martin.cabrera-x@unqork.com>
2025-03-20 19:26:37 -0400

    New Initial

---
+++ step_definitions/steps.ts
@@ -0,0 +1,24 @@
+const { I } = inject();
+const mongoInstance = require("../helpers/mongoSingleton");
+const {ok} = require("assert");
+// Add in your custom step files
+
+Given('I have a defined step', () => {
+    // TODO: replace with your own step
+    //I.amOnPage('/');
+    I.wait(5);
+    I.
+});
+When(/^I do something$/, async function () {
+
+});
+Then(/^I expect a result$/, function () {
+
+});
+When(/^I verify my bd that my collection is not empty$/, async function () {
+    const db = await mongoInstance.connect();
+    const testCollection = mongoInstance.getCollection("person");
+    console.log(testCollection);
+    const result = await testCollection.find().toArray();
+    ok(result.length > 0);
+});
